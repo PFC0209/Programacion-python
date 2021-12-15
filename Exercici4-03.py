6016895428974c633622e6caffbe0d8929be51d3
PFC0209 <93318970+PFC0209@users.noreply.github.com>
2021-12-15 10:39:07 +0100

    Create Exercici4-03.py

---
+++ Exercici4-03.py
@@ -0,0 +1,7 @@
+entrada_usuario = input("Una cereza es:")
+#A = input("La guinda del postre")
+#B = input("La guinda del desierto")
+if entrada_usuario == "A":
+    print("Correcto!")
+else:
+    print("No es correcto")
