0fe3ffa095b67cdc3134a219922636d5a38c5d3b
PFC0209 <93318970+PFC0209@users.noreply.github.com>
2021-12-20 10:46:20 +0100

    Add files via upload

---
+++ Exercici8-04.py
@@ -0,0 +1,15 @@
+print("Este programa recibe siet números y devuelve la suma.")
+total = 0
+ 
+for i in range(7):
+    x = input("Introduce un número: ")
+    total = total + float(x)
+print("El total es:", total)
+"""
+if x > 0:   
+    print("El total es:", total)
+elif x < 0:
+    print("El total es:", total)
+elif x == 0:
+    print("El total es:", total)
+"""
