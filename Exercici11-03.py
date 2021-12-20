1f391f20d23397f0666d62f8860cc6b6eb6a4d92
PFC0209 <93318970+PFC0209@users.noreply.github.com>
2021-12-20 09:59:22 +0100

    Add files via upload

---
+++ Exercici11-03.py
@@ -0,0 +1,12 @@
+print("3" == "3")
+print(" 3" == "3")
+print(3 < 4)
+print(3 < 10)
+print("3" < "4")
+print("3" < "10")
+print( (2 == 2) == "True" )
+print( (2 == 2) == True )
+print(3 < "3")
+"""
+El programa no podrà fer res perquè no té ninguna variable
+"""
