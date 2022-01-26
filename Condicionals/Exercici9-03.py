87883ef9ba1f9e1aae97afe81399b203c6cd967f
PFC0209 <93318970+PFC0209@users.noreply.github.com>
2022-01-26 10:18:20 +0100

    Create Exercici9-03.py

---
+++ Condicionals/Exercici9-03.py
@@ -0,0 +1,16 @@
+x = 5
+y = x == 6
+z = x == 5
+print("x=", x)
+print("y=", y)
+print("z=", z)
+if y:
+    print ("Fizz")
+if z:
+    print ("Buzz")
+
+
+"""
+El programa fa que si la teva variable x és igual 5 i y es igual igual a 6 el program te imprimirà Fizz i també
+si z igual igual a 5 el programa te imprimirà un Buzz.
+"""
