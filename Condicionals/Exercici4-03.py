2efcc5839726e06fae3cd260754ef7a483e1d9a5
PFC0209 <93318970+PFC0209@users.noreply.github.com>
2022-01-26 10:13:43 +0100

    Create Exercici4-03.py

---
+++ Condicionals/Exercici4-03.py
@@ -0,0 +1,7 @@
+A = input("La guinda del postre")
+B = input("La guinda del desierto")
+entrada_usuario = input("Una cereza es:")
+if entrada_usuario == "A":
+    print("Correcto!")
+else:
+    print("No es correcto")
